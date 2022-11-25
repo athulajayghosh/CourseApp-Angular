0e9f872294d50db56fdac0f338d0180f0af59fe7
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-11-25 17:20:14 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
+
+const myRoute: Routes=[
+  {path:"",
+  component:AddCourseComponent
+  },
+  {path:"viewall",
+  component:ViewCourseComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AddCourseComponent,
+    ViewCourseComponent
   ],
   imports: [
     BrowserModule,
